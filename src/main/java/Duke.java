e0f714acfc05aa25fe0399913128c5b305983ba1
Jefferson <e0895782@u.nus.edu>
2024-01-29 00:12:51 +0800

    change name function works

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,15 @@
 import java.lang.System;
 import java.util.Scanner;
+import java.util.NoSuchElementException;
+
 
 public class Duke {
     private String chatBotName = "Jenkins";
+    public static String userInput = "";
 
     public Duke(){
         String chatBotName = "Jenkins";
-        System.out.println("Hello! you may call me " + getChatBotName());
-        System.out.println("What can I do for you?");
-        drawLine();
+        chatBotGreetings();
         listenForInput();
     }
 
@@ -16,36 +17,83 @@ public class Duke {
         return this.chatBotName;
     }
 
-    public void stopProgram(){
-        System.out.print(getChatBotName() + ": Bye. Hope to see you again soon!\n");
-        drawLine();
+    public void setChatBotName(String userInput){
+        this.chatBotName = userInput;
     }
 
-    public void listenForInput(){
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        drawLine();
+    public void chatBotGreetings(){
+        System.out.println(getChatBotName() + ": Hello! you may call me " + getChatBotName() + ". What can I do for you?");
+    }
 
-        if (userInput.equals("bye")){
+    public void stopProgram(){
+        System.out.print(getChatBotName() + ": Bye. Hope to see you again soon!\n");
+    }
+
+    //Top cases are corner cases. Bottom for future
+    public void chatBotFunctions(String userInput){
+        //case 1: stop program
+        if (userInput.equals(getChatBotName() + ": bye")){
             stopProgram();
             return;
         }
 
-        response(userInput);
-        sc.close();
+        //case 2: change chat-bot name
+        else if (userInput.equals("change bot name")){
+            System.out.println(getChatBotName() + ": Sure! Please key in my new name");
+            Scanner sc = new Scanner(System.in); //open scanner!
+            userInput = sc.nextLine();
+
+            String name = userInput.trim();
+            setChatBotName(name);
+
+            System.out.println(getChatBotName() + ": Right away!");
+            chatBotGreetings();
+            return;
+        }
+
+//        //case: future implementations
+//        else if (true){
+//            //future implementations do something
+//        }
+
+        //Last case: echos user
+        else {
+            echoUserInput(userInput);
+            return;
+        }
+
     }
 
-    public void response(String s){ //echos
-       System.out.println(s);
+    public void listenForInput() {
         drawLine();
+        Scanner sc = new Scanner(System.in);
+
+        userInput  = sc.nextLine();
+
+         if (userInput.isEmpty()) {
+             System.out.println("Sorry, I did not receive any commands");
+             System.out.println("Please type something to let me understand");
+             listenForInput();
+        }
+         else {
+             chatBotFunctions(userInput);
+         }
+    }
+
+
+    public void echoUserInput(String s){
+       System.out.println(s);
        listenForInput();
     }
 
-    public void drawLine(){
+    public void drawLine() {
         System.out.println("____________________________________________________________");
     }
 
+
     public static void main(String[] args) {
         Duke Jenkins = new Duke();
+        Jenkins.listenForInput();
     }
+
 }
