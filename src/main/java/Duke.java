52ba52aa8ece3a67f6dd0f9adee83858822eac6b
Joel Ee <joelwhee@gmail.com>
2024-01-31 20:25:48 +0800

    Level - 0 Implementation (1) Assigned a variable to hold chatbot name (2) Create 2 print statements (3) Call System exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
+        String chatbotname = "Sid";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.printf("Hello! I'm %s \nWhat can I do for you?\n", chatbotname);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.exit(0);
+
     }
 }
