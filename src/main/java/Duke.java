4dc04e09e5bf23cea772d17ca256bd14d100f7f0
mavericktjj <64763339+mavericktjj@users.noreply.github.com>
2024-01-31 20:12:49 +0800

    Update Duke.java
    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Maverick");
+        System.out.println("What can i do for you?");
+
+
+        System.out.println("Bye. Hope to see you again.");
+//        while (true) {
+//
+//
+//
+//            Scanner in = new Scanner(System.in);
+//            String input = in.nextLine;
+//            if (input.equals("bye")) {
+//                break;
+//            }
+//            System.out.printf("Hello! I'm %s", line);
+//
+//
+//
+//        }
     }
 }
