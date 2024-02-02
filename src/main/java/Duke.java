4dcd66df8da51a359aa7af74a7f7a7f3949886a4
anythingwhatever192940958 <e0895898@u.nus.edu>
2024-02-02 16:09:58 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,11 +10,28 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "\n");
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Fujin");
-        System.out.println(" What can I do for you?");
+        System.out.println("Hello! I'm Fujin");
+        System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            // read
+            String userInput = scanner.nextLine();
+
+            // if bye
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break; // Exit the loop
+            }
+
+            // else echo
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
