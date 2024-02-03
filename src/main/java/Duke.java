93432d20257f302e70fc788f8d7d4b9d268d9f91
mfarhan-s <farhansalim26@gmail.com>
2024-02-03 14:31:12 +0800

    add runDuke, echoUserInput, printHorizontalLine edit greetUser, sayGoodbye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
-
-    public static void greetUser() {
-        System.out.println("Hello! I'm Balrog.");
-        System.out.println("What can I do for you?");
-    }
-
-    public static void sayGoodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-
     public static void main(String[] args) {
         greetUser();
+        runDuke();
         sayGoodbye();
+    }
+    public static void greetUser() {
+        System.out.println("Greetings, mortal! I am Balrog, the fiery demon.");
+        System.out.println("What foolish command do you wish to utter?");
+        printHorizontalLine();
+    }
+    public static void runDuke() {
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            userInput = in.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                continue;
+            }
+            echoUserInput(userInput);
+            printHorizontalLine();
+        } while (!userInput.equalsIgnoreCase("bye"));
+
+    }
+    public static void sayGoodbye() {
+        System.out.println("    Flee, mortal! Until our paths cross again!");
+        printHorizontalLine();
+    }
+    public static void printHorizontalLine() {
+        System.out.println("--------------------------------");
+    }
+    public static void echoUserInput(String input) {
+        System.out.println("    " + input);
     }
 }
