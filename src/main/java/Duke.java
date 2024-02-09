0fe3861f6c022a5cbd04f47b3dde61850320063f
jelemii <xxjeremy2xx@gmail.com>
2024-02-10 03:55:07 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greetUser = "Hello! I'm jelemiiBot\n"
                 + "What can I do for you?\n";
-        String exit = "Bye. Hope to see you again soon!";
         System.out.println(greetUser);
+
+        String line = "";
+        String bye = "bye";
+        Scanner in = new Scanner(System.in);
+
+        do {
+            line = in.nextLine();
+            System.out.println(line);
+        } while (!line.equals(bye));
+
+        String exit = "Bye. Hope to see you again soon!";
+
         System.out.println(exit);
     }
 }
