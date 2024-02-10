cf84ed25f695af61ad708d8d0599c6e3ad5e3d2a
jelemii <xxjeremy2xx@gmail.com>
2024-02-11 00:54:15 +0800

    Level 2. Add, List
    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,17 +6,35 @@ public class Duke {
                 + "What can I do for you?\n";
         System.out.println(greetUser);
 
-        String line = "";
-        String bye = "bye";
+        String input = "";
+        String[] inputList = new String[100];
+        int index = 0;
         Scanner in = new Scanner(System.in);
 
         do {
-            line = in.nextLine();
-            System.out.println(line);
-        } while (!line.equals(bye));
+            input = in.nextLine();
 
-        String exit = "Bye. Hope to see you again soon!";
+            if (input.equals("list")) {
+                if(index == 0) {
+                    System.out.println("List is empty");
+                }
+                else {
+                    for(int i = 0; i < index; i++) {
+                        System.out.println((i+1) + ". " + inputList[i]);
+                    }
+                }
+            }
+            else if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+            }
 
-        System.out.println(exit);
+            else {
+                inputList[index] = input;
+                index++;
+                System.out.println("added: " + input);
+            }
+
+        } while (!input.equals("bye"));
+
     }
 }
