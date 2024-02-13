811937fb6893ad14b65ba98f63428464c1e5f8fa
mfarhan-s <farhansalim26@gmail.com>
2024-02-13 20:55:09 +0800

    Add a new class named Task, edited Main to utilise new class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int MAX_TASKS = 100;
-    private static String[] taskList = new String[MAX_TASKS];
+    private static Task[] taskList = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
@@ -14,6 +14,80 @@ public class Duke {
         System.out.println("Greetings, mortal! I am Balrog, the fiery demon.");
         System.out.println("What foolish commands do you wish to utter?");
         printHorizontalLine();
+    }
+    public static void sayGoodbye() {
+        System.out.println("    Flee, mortal! Until our paths cross again!");
+        printHorizontalLine();
+    }
+    public static void printHorizontalLine() {
+        System.out.println("-------------------------------------------------");
+    }
+    public static void addTaskToList(Task task) {
+        if (taskCount < MAX_TASKS) {
+            taskList[taskCount] = task;
+            taskCount++;
+            echoUserCommand(task.getDescription());
+        } else {
+            System.out.println("    The list of tasks is full! I shall not be burdened further.");
+        }
+    }
+    public static void displayList() {
+        System.out.println("======= Scroll of Puny Tasks =======");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("    " + (i + 1) + ". " + taskList[i]);
+        }
+        printHorizontalLine();
+    }
+    public static void echoUserCommand(String command) {
+        System.out.println("    A foolish command, mortal! You dare to utter: \"" + command + "\"");
+    }
+    public static void executeCommand(String command) {
+        String[] commandParts = command.split(" ", 10);
+
+        switch (commandParts[0].toLowerCase()) {
+            case "list":
+                displayList();
+                break;
+            case "mark":
+                if (commandParts.length > 1) {
+                    markTaskAsDone(Integer.parseInt(commandParts[1]));
+                } else {
+                    System.out.println("    Specify the task number to mark as done.");
+                }
+                break;
+            case "unmark":
+                if (commandParts.length > 1) {
+                    unmarkTaskAsDone(Integer.parseInt(commandParts[1]));
+                } else {
+                    System.out.println("    Specify the task number to mark as not done.");
+                }
+                break;
+            default:
+                addTaskToList(new Task(command));
+        }
+    }
+    public static void markTaskAsDone(int taskNumber) {
+        if (isValidTaskNumber(taskNumber)) {
+            taskList[taskNumber - 1].markAsDone();
+            System.out.println("    Hmph! I've smitten this task from the list:\n      [" +
+                                        taskList[taskNumber - 1].getStatusIcon() + "] " +
+                                            taskList[taskNumber - 1].getDescription());
+        } else {
+            System.out.println("    Fool! That task number is beyond the realm of your pitiful list!");
+        }
+    }
+    public static void unmarkTaskAsDone(int taskNumber) {
+        if (isValidTaskNumber(taskNumber)) {
+            taskList[taskNumber - 1].unmarkAsDone();
+            System.out.println("    Bah! I've restored this task to its pathetic existence:\n    [" +
+                                        taskList[taskNumber - 1].getStatusIcon() + "] " +
+                                            taskList[taskNumber - 1].getDescription());
+        } else {
+            System.out.println("    You dare invoke the invalid task number? Pathetic!");
+        }
+    }
+    public static boolean isValidTaskNumber(int taskNumber) {
+        return taskNumber > 0 && taskNumber <= taskCount;
     }
     public static void runDuke() {
         String userInput;
@@ -27,35 +101,8 @@ public class Duke {
                 displayList();
                 continue;
             }
-            echoUserInput(userInput);
-            addInputToList(userInput);
+            executeCommand(userInput);
             printHorizontalLine();
         } while (!userInput.equalsIgnoreCase("bye"));
-
-    }
-    public static void sayGoodbye() {
-        System.out.println("    Flee, mortal! Until our paths cross again!");
-        printHorizontalLine();
-    }
-    public static void printHorizontalLine() {
-        System.out.println("--------------------------------");
-    }
-    public static void echoUserInput(String input) {
-        System.out.println("     A feeble command, mortal! You dare to decree: \"" + input + "\"");
-    }
-    public static void addInputToList(String input) {
-        if (taskCount < MAX_TASKS) {
-            taskList[taskCount] = input;
-            taskCount++;
-        } else {
-            System.out.println("    The list of tasks is full! I shall not be burdened further.");
-        }
-    }
-    public static void displayList() {
-        System.out.println("=== Scroll of Puny Tasks ===");
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println("    " + (i+1) + ". " + taskList[i]);
-        }
-        printHorizontalLine();
     }
 }
