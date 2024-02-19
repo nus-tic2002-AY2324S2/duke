864b1fc406901dbc5bf78e6a6e0912208a033238
jelemii <xxjeremy2xx@gmail.com>
2024-02-19 18:49:23 +0800

    Level 3. Mark As Done
    added task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,67 @@
 import java.util.Scanner;
 
 public class Duke {
+    protected static Task[] taskList = new Task[100];
+    protected static int index = 0;
+
+    public static void addTask(String input) {
+        taskList[index++] = new Task(input);
+        System.out.println("added: " + input);
+    }
+
     public static void main(String[] args) {
         String greetUser = "Hello! I'm jelemiiBot\n"
                 + "What can I do for you?\n";
         System.out.println(greetUser);
 
-        String input = "";
-        String[] inputList = new String[100];
-        int index = 0;
+        boolean getInput = true;
         Scanner in = new Scanner(System.in);
 
-        do {
-            input = in.nextLine();
+        while (getInput) {
+            String input = in.nextLine().trim();
 
             if (input.equals("list")) {
                 if(index == 0) {
                     System.out.println("List is empty");
                 }
                 else {
-                    for(int i = 0; i < index; i++) {
-                        System.out.println((i+1) + ". " + inputList[i]);
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < index; i++) {
+                        System.out.println((i+1) + ". " + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
                     }
                 }
             }
             else if(input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+                getInput = false;
             }
-
+            else if(input.startsWith("mark")){
+                int getIndex = Integer.parseInt(input.substring(5)) - 1;
+                if(getIndex >= 0 && getIndex < index) {
+                    Task task = taskList[getIndex];
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("  [" + taskList[getIndex].getStatusIcon() + "] " + taskList[getIndex].description);
+                } else {
+                    System.out.println("Invalid index");
+                }
+            }
+            else if(input.startsWith("unmark")){
+                int getIndex = Integer.parseInt(input.substring(7)) - 1;
+                if(getIndex >= 0 && getIndex < index) {
+                    Task task = taskList[getIndex];
+                    task.unmarkAsDone();
+                    System.out.println("OK, I've marked this task as not done yet: ");
+                    System.out.println("  [" + taskList[getIndex].getStatusIcon() + "] " + taskList[getIndex].description);
+                } else {
+                    System.out.println("Invalid index");
+                }
+            }
             else {
-                inputList[index] = input;
-                index++;
-                System.out.println("added: " + input);
+                addTask(input);
             }
 
-        } while (!input.equals("bye"));
+        }
 
     }
 }
