4e9c2a903bbc46c0368f38c5b4b4f7a669a3bed1
jelemii <xxjeremy2xx@gmail.com>
2024-02-27 16:19:16 +0800

    Level 4. ToDos, Events, Deadlines
    add support for tracking three types of task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,25 @@ public class Duke {
     protected static Task[] taskList = new Task[100];
     protected static int index = 0;
 
-    public static void addTask(String input) {
-        taskList[index++] = new Task(input);
-        System.out.println("added: " + input);
+    public static void addTodo(String input) {
+        taskList[index++] = new Todo(input);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + taskList[index - 1].toString());
+        System.out.println("Now you have " + index + " tasks in the list.");
+    }
+
+    public static void addDeadline(String desc, String by) {
+        taskList[index++] = new Deadline(desc, by);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + taskList[index - 1].toString());
+        System.out.println("Now you have " + index + " tasks in the list.");
+    }
+
+    public static void addEvent(String desc, String from, String to) {
+        taskList[index++] = new Event(desc, from, to);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + taskList[index - 1].toString());
+        System.out.println("Now you have " + index + " tasks in the list.");
     }
 
     public static void main(String[] args) {
@@ -27,7 +43,7 @@ public class Duke {
                 else {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < index; i++) {
-                        System.out.println((i+1) + ". " + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
+                        System.out.println((i+1) + "." + taskList[i].toString());
                     }
                 }
             }
@@ -41,7 +57,7 @@ public class Duke {
                     Task task = taskList[getIndex];
                     task.markAsDone();
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println("  [" + taskList[getIndex].getStatusIcon() + "] " + taskList[getIndex].description);
+                    System.out.println(taskList[getIndex].toString());
                 } else {
                     System.out.println("Invalid index");
                 }
@@ -52,15 +68,31 @@ public class Duke {
                     Task task = taskList[getIndex];
                     task.unmarkAsDone();
                     System.out.println("OK, I've marked this task as not done yet: ");
-                    System.out.println("  [" + taskList[getIndex].getStatusIcon() + "] " + taskList[getIndex].description);
+                    System.out.println(taskList[getIndex].toString());
                 } else {
                     System.out.println("Invalid index");
                 }
             }
-            else {
-                addTask(input);
+            else if(input.startsWith("todo")) {
+                addTodo(input);
             }
-
+            else if(input.startsWith("deadline")) {
+                int byIndex = input.indexOf("/by");
+                String desc = input.substring(9, byIndex).trim();
+                String by = input.substring(byIndex + 3).trim();
+                addDeadline(desc, by);
+            }
+            else if(input.startsWith("event")) {
+                int fromIndex = input.indexOf("/from");
+                int toIndex = input.indexOf("/to");
+                String desc = input.substring(6, fromIndex).trim();
+                String from = input.substring(fromIndex + 5, toIndex).trim();
+                String to = input.substring(toIndex + 3).trim();
+                addEvent(desc, from, to);
+            }
+            else {
+                System.out.println("Please input a valid task");
+            }
         }
 
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Todo {
+public class Todo extends Task {
+
+    public Todo(String description) {
+        super(description.substring(5));
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
+
