80630f15d4bfc40317f93b18979223915044ae7a
jelemii <xxjeremy2xx@gmail.com>
2024-03-07 00:41:50 +0800

    add try catch and created dukeException class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -36,63 +36,74 @@ public class Duke {
         while (getInput) {
             String input = in.nextLine().trim();
 
-            if (input.equals("list")) {
-                if(index == 0) {
-                    System.out.println("List is empty");
-                }
-                else {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < index; i++) {
-                        System.out.println((i+1) + "." + taskList[i].toString());
+            try {
+                if (input.equals("list")) {
+                    if(index == 0) {
+                        System.out.println("List is empty");
+                    }
+                    else {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < index; i++) {
+                            System.out.println((i+1) + "." + taskList[i].toString());
+                        }
                     }
                 }
-            }
-            else if(input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                getInput = false;
-            }
-            else if(input.startsWith("mark")){
-                int getIndex = Integer.parseInt(input.substring(5)) - 1;
-                if(getIndex >= 0 && getIndex < index) {
-                    Task task = taskList[getIndex];
-                    task.markAsDone();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(taskList[getIndex].toString());
-                } else {
-                    System.out.println("Invalid index");
+                else if(input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    getInput = false;
                 }
-            }
-            else if(input.startsWith("unmark")){
-                int getIndex = Integer.parseInt(input.substring(7)) - 1;
-                if(getIndex >= 0 && getIndex < index) {
-                    Task task = taskList[getIndex];
-                    task.unmarkAsDone();
-                    System.out.println("OK, I've marked this task as not done yet: ");
-                    System.out.println(taskList[getIndex].toString());
-                } else {
-                    System.out.println("Invalid index");
+                else if(input.startsWith("mark")){
+
+                    try {
+                        int getIndex = Integer.parseInt(input.substring(5)) - 1;
+                        if(getIndex >= 0 && getIndex < index) {
+                            Task task = taskList[getIndex];
+                            task.markAsDone();
+                            System.out.println("Nice! I've marked this task as done: ");
+                            System.out.println(taskList[getIndex].toString());
+                        } else {
+                            System.out.println("Invalid index");
+                        }
+                    } catch (NumberFormatException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+                else if(input.startsWith("unmark")){
+                    int getIndex = Integer.parseInt(input.substring(7)) - 1;
+                    if(getIndex >= 0 && getIndex < index) {
+                        Task task = taskList[getIndex];
+                        task.unmarkAsDone();
+                        System.out.println("OK, I've marked this task as not done yet: ");
+                        System.out.println(taskList[getIndex].toString());
+                    } else {
+                        System.out.println("Invalid index");
+                    }
+                }
+                else if(input.startsWith("todo")) {
+                    addTodo(input);
+                }
+                else if(input.startsWith("deadline")) {
+                    int byIndex = input.indexOf("/by");
+                    String desc = input.substring(9, byIndex).trim();
+                    String by = input.substring(byIndex + 3).trim();
+                    addDeadline(desc, by);
+                }
+                else if(input.startsWith("event")) {
+                    int fromIndex = input.indexOf("/from");
+                    int toIndex = input.indexOf("/to");
+                    String desc = input.substring(6, fromIndex).trim();
+                    String from = input.substring(fromIndex + 5, toIndex).trim();
+                    String to = input.substring(toIndex + 3).trim();
+                    addEvent(desc, from, to);
+                }
+                else {
+                    throw new DukeException();
+                }
+            } catch (DukeException e) {
+                System.out.println("Please input a valid task.");
             }
-            else if(input.startsWith("todo")) {
-                addTodo(input);
-            }
-            else if(input.startsWith("deadline")) {
-                int byIndex = input.indexOf("/by");
-                String desc = input.substring(9, byIndex).trim();
-                String by = input.substring(byIndex + 3).trim();
-                addDeadline(desc, by);
-            }
-            else if(input.startsWith("event")) {
-                int fromIndex = input.indexOf("/from");
-                int toIndex = input.indexOf("/to");
-                String desc = input.substring(6, fromIndex).trim();
-                String from = input.substring(fromIndex + 5, toIndex).trim();
-                String to = input.substring(toIndex + 3).trim();
-                addEvent(desc, from, to);
-            }
-            else {
-                System.out.println("Please input a valid task");
-            }
+
+
         }
 
     }
